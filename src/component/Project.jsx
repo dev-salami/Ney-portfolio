e255274bec3e45b150c67ded0cbf04fcf3967bea
khalil <salamikhalil02@gmail.com>
2023-06-09 09:51:47 +0100

    project and font update

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -4,49 +4,72 @@ import { useContext } from "react";
 
 import Shared from "./shared";
 function Project() {
-  const { Light } = useContext(DataContext);
+	const { Light } = useContext(DataContext);
 
-  return (
-    <div className="h-fit">
-      <Shared />
-      <div
-        className={
-          !Light
-            ? "bg-[#fbdede]  all text-black  pt-32 px-12"
-            : "bg-[#0a192f] all text-white  pt-32  px-12"
-        }
-      >
-        <div className="container mx-auto flex justify-center pb-20">
-          <div className="lg:grid-cols-2 grid-cols-1 grid lg:gap-4 gap-16 w-fit  items-center text ">
-            <Pitem
-              image={require("../assets/artsy.png")}
-              title="Artsy"
-              details=" Arsty is a site for vintage shots, historic images and captures seamlessly in one place."
-              stack="React js || Tailwind || Aos || PayStack"
-              link="https://arty.vercel.app"
-            />
-            <Pitem
-              image={require("../assets/port-image.png")}
-              title="Portfolio"
-              details=" This is my Personal Portfolio"
-              stack="React js || Tailwind || Framer Motion"
-              link="https://salami.ng"
-            />
-            <Pitem
-              image={require("../assets/planet.png")}
-              title="Planet Facts"
-              details=" Fact site for all planets in our solar system."
-              stack="React js || Tailwind || Framer Motion"
-              link="https://planet-facts-ney.vercel.app"
-            />
-            <Pitem
-              image={require("../assets/crappo.png")}
-              title="Crappo"
-              details="A Crypto Currency Website"
-              stack="React js || Tailwind || AOS"
-              link="https://crappo-ney.vercel.app"
-            />
-            {/* <Pitem
+	return (
+		<div className="h-fit">
+			<Shared />
+			<div
+				className={
+					!Light
+						? "bg-[#fbdede]  all text-black  pt-32 px-12"
+						: "bg-[#0a192f] all text-white  pt-32  px-12"
+				}>
+				<div className="container mx-auto flex justify-center pb-20">
+					<div className="lg:grid-cols-2 grid-cols-1 grid gap-8 w-fit  items-center text-sm ">
+						<Pitem
+							image={require("../assets/audiophile.png")}
+							title="Audiophile"
+							details="Audiophile is an e-commerce website for audio accessories. It allows users to explore and purchase a wide range of high-quality audio products.
+              "
+							stack="Next Js || JavaScript || Next Auth || Tailwind || AOS || PayStack"
+							link="https://audiophile-ney.vercel.app"
+						/>
+						<Pitem
+							image={require("../assets/artsy.png")}
+							title="Artsy"
+							details=" Arsty is an e-commerce website for vintage shots, historic images and captures seamlessly in one place."
+							stack="React js || Tailwind || Aos || PayStack"
+							link="https://arty.vercel.app"
+						/>
+						<Pitem
+							image={require("../assets/kwems.png")}
+							title="Kwara Electoral Management System"
+							details="Kwara Electoral Management System is an election result portal for Kwara State
+              "
+							stack="React js || Tailwind "
+							link="https://kwems.vercel.app"
+						/>
+						<Pitem
+							image={require("../assets/arch.png")}
+							title="Arch Studio"
+							details="Arch Studio is an architectural portfolio website"
+							stack="React js || Tailwind || Aos"
+							link="https://arch-ney.vercel.app"
+						/>
+
+						<Pitem
+							image={require("../assets/port-image.png")}
+							title="Portfolio"
+							details=" This is my Personal Portfolio"
+							stack="React js || Tailwind || Framer Motion"
+							link="https://salami.ng"
+						/>
+						<Pitem
+							image={require("../assets/planet.png")}
+							title="Planet Facts"
+							details=" Fact site for all planets in our solar system."
+							stack="React js || Tailwind || Framer Motion"
+							link="https://planet-facts-ney.vercel.app"
+						/>
+						<Pitem
+							image={require("../assets/crappo.png")}
+							title="Crappo"
+							details="A Crypto Currency Website "
+							stack="React js || Tailwind || AOS"
+							link="https://crappo-ney.vercel.app"
+						/>
+						{/* <Pitem
               image={require("../assets/farm.png")}
               title="WH Farms"
               details="A Farm Ecommerce website "
@@ -54,52 +77,46 @@ function Project() {
               link="https://whfarms.vercel.app"
             /> */}
 
-            <Pitem
-              image={require("../assets/news.png")}
-              title="News App"
-              details="News App using a news api"
-              stack="React || Tailwind || Axios"
-              link="https://ney-news.vercel.app"
-            />
-            <Pitem
-              image={require("../assets/movie-app.png")}
-              title="Ney-Movies"
-              details=" A movie website using the movie database api"
-              stack="React js || Tailwind "
-              link="https://ney-movies.vercel.app"
-            />
-            <Pitem
-              image={require("../assets/audiophile.png")}
-              title="Audiophile"
-              details=" An Ecommerce Website"
-              stack="React js || Tailwind || React Reveal"
-              link="https://audiophile-ney.vercel.app"
-            />
-            <Pitem
-              image={require("../assets/cocktail.png")}
-              title="Cocktail"
-              details="A cocktail website using the cocktail db ap1 "
-              stack="React js || Tailwind "
-              link="https://cocktail-ney.vercel.app"
-            />
-            {/* <Pitem
+						<Pitem
+							image={require("../assets/news.png")}
+							title="News App"
+							details="News App using a news api"
+							stack="React || Tailwind || Axios"
+							link="https://ney-news.vercel.app"
+						/>
+						<Pitem
+							image={require("../assets/movie-app.png")}
+							title="Ney-Movies"
+							details=" A movie website using the movie database api"
+							stack="React js || Tailwind "
+							link="https://ney-movies.vercel.app"
+						/>
+
+						<Pitem
+							image={require("../assets/cocktail.png")}
+							title="Cocktail"
+							details="A cocktail website using the cocktail db ap1 "
+							stack="React js || Tailwind "
+							link="https://cocktail-ney.vercel.app"
+						/>
+						{/* <Pitem
               image={require("../assets/furniture.png")}
               title="Furniture Store"
               details=" A Website for a furniture store"
               stack="React || Tailwind"
               link="https://ney-furniture.vercel.app"
             /> */}
-            {/* <Pitem
+						{/* <Pitem
               image={require("../assets/graphics-resume.png")}
               title="Portfolio"
               details=" A portfolio for a graphics designer."
               stack="Html || Sass || javascript"
               link="https://froshgraphics.netlify.app"
             /> */}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 export default Project;
--- src/component/pitem.jsx
+++ src/component/pitem.jsx
@@ -3,80 +3,82 @@ import DataContext from "../context/DataContext";
 import { FaGithub } from "react-icons/fa";
 
 function Pitem(props) {
-  const { Light } = useContext(DataContext);
+	const { Light } = useContext(DataContext);
 
-  return (
-    <div>
-      <div className="hidden sm:flex font-semibold">
-        <div className="    w-[450px] h-[250px] card rounded-md">
-          <div
-            //  className="card-side front   shadow-sm border-2 border-black    rounded-md p-2 "
-            className={
-              Light
-                ? "card-side front   shadow-sm border-2 border-gray-100    rounded-md p-2 "
-                : "card-side front   shadow-sm border-2 border-black    rounded-md p-2 "
-            }
-          >
-            <img className="w-full h-full" src={props.image} alt="" />
-          </div>
-          <div className="card-side back w-full h-fit text-xl font-thin rounded-md p-3">
-            <div
-              className={
-                Light
-                  ? "bg-black p-4 shadow-md shadow-gray-400  rounded-xl flex flex-col space-y-3"
-                  : "bg-gray-300 p-4 shadow-md shadow-black rounded-xl flex flex-col space-y-3"
-              }
-            >
-              <h2>{props.title}</h2>
-              <p>{props.details}</p>
-              <p className="uppercase">{props.stack}</p>
-              <div className="flex font-bold justify-between">
-                <button
-                  className={
-                    Light
-                      ? "text-black bg-white px-2 py-1 rounded-md hover:bg-opacity-70 duration-300"
-                      : "bg-white px-2 py-1 rounded-md hover:bg-opacity-70 duration-300"
-                  }
-                >
-                  <a href={props.link}>Demo</a>
-                </button>
-                <a
-                  className={Light ? "text-white " : "text-black "}
-                  href="https://github.com/Skhalil772"
-                >
-                  <FaGithub size={30} />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="flex shadow-md shadow-gray-100 sm:hidden flex-col space-y-6 font-semibold justify-around h-fit w-[300px] bg-gray-600 rounded-md p-4 ">
-          <img className="w-fit " src={props.image} alt="" />
-          <h2>{props.title}</h2>
-          <p>{props.details}</p>
-          <p>{props.stack}</p>
-          <div className="flex font-bold justify-between">
-            <button
-              className={
-                Light
-                  ? "bg-black px-2 py-1 rounded-md hover:bg-opacity-70 duration-300"
-                  : "bg-white px-2 py-1 rounded-md hover:bg-opacity-70 duration-300"
-              }
-            >
-              <a href={props.link}>Demo</a>
-            </button>
-            <a
-              className={Light ? "text-black " : "text-white "}
-              href="https://github.com/Skhalil772"
-            >
-              <FaGithub size={30} />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="hidden sm:flex font-semibold">
+				<div className="    w-[450px] h-[250px] card rounded-md">
+					<div
+						//  className="card-side front   shadow-sm border-2 border-black    rounded-md p-2 "
+						className={
+							Light
+								? "card-side front   shadow-sm border-2 border-gray-100    rounded-md p-2 "
+								: "card-side front   shadow-sm border-2 border-black    rounded-md p-2 "
+						}>
+						<img
+							className="w-full h-full"
+							src={props.image}
+							alt=""
+						/>
+					</div>
+					<div className="card-side back w-full h-fit text-xl font-thin rounded-md p-3">
+						<div
+							className={
+								Light
+									? "bg-black p-4 shadow-md shadow-gray-400  rounded-xl flex flex-col space-y-3"
+									: "bg-gray-300 p-4 shadow-md shadow-black rounded-xl flex flex-col space-y-3"
+							}>
+							<h2>{props.title}</h2>
+							<p>{props.details}</p>
+							<p className="text-xs uppercase">{props.stack}</p>
+							<div className="flex font-bold justify-between">
+								<button
+									className={
+										Light
+											? "text-black bg-white px-2 py-1 rounded-md hover:bg-opacity-70 duration-300"
+											: "bg-white px-2 py-1 rounded-md hover:bg-opacity-70 duration-300"
+									}>
+									<a href={props.link}>Demo</a>
+								</button>
+								<a
+									className={Light ? "text-white " : "text-black "}
+									href="https://github.com/Skhalil772">
+									<FaGithub size={30} />
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div>
+				<div className="flex shadow-md shadow-gray-100 sm:hidden flex-col space-y-6 font-semibold justify-around h-fit w-[300px] bg-gray-600 rounded-md p-4 ">
+					<img
+						className="w-fit "
+						src={props.image}
+						alt=""
+					/>
+					<h2>{props.title}</h2>
+					<p>{props.details}</p>
+					<p>{props.stack}</p>
+					<div className="flex font-bold justify-between">
+						<button
+							className={
+								Light
+									? "bg-black px-2 py-1 rounded-md hover:bg-opacity-70 duration-300"
+									: "bg-white px-2 py-1 rounded-md hover:bg-opacity-70 duration-300"
+							}>
+							<a href={props.link}>Demo</a>
+						</button>
+						<a
+							className={Light ? "text-black " : "text-white "}
+							href="https://github.com/Skhalil772">
+							<FaGithub size={30} />
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 export default Pitem;
